0a3a0d746857562dd9af98ed7ee09d6ffa346b68
KevanshGopani <kevanshgopani5@gmail.com>
2024-07-19 22:35:53 +0530

    added image file upload functionality and create simple user registration route

--- src/app.js
+++ src/app.js
@@ -26,4 +26,12 @@ app.use(
 );
 
 app.use(express.static("public"));
+app.use(cookieParser());
+
+//Routes
+import userRouter from "./routes/user.routes.js";
+
+//Routes declaration
+app.use("/api/v1/users", userRouter);
+
 export { app };
--- src/db/index.js
+++ src/db/index.js
@@ -4,10 +4,14 @@ import { DB_NAME } from "../constants.js";
 const connectDatabase = async () => {
   try {
     const connectionInstance = await mongoose.connect(
-      `${process.env.MONGODB_URI}/${DB_NAME}`
+      `${process.env.MONGODB_URI}/${DB_NAME}`,
+      {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+      }
     );
     if (connectionInstance) {
-      console.log(`Mongodb connected port ${process.env.PORT}`);
+      console.log(`Mongodb connected successfully`);
     }
   } catch (error) {
     console.error("Mongodb connection error", error);
